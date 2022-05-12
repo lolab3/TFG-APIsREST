bcefca1c532239c4c368f1cf84e69db91de725c9
Lola Barberan Baeta <lolabarberan1@gmail.com>
2022-05-12 23:25:05 +0200

    detection

---
+++ example.py
@@ -0,0 +1,17 @@
+user1 = [
+    {
+        "get_api_v1_admin": [{"parameters": [""]}, {"response": "200"}],
+        "get_api_v1_hospital_hospital_id_department_department_id": [{"parameters": ["hospital","department"]}, {"response": "200"},],
+        "get_api_v1_hospital_hospital_id": [{"parameters": ["hospital"]}, {"response": "200"},],
+        " post_api_v1_patient": [{"parameters": ["patient"]}, {"response": "200"},],
+        "put_api_v1_patient": [{"parameters": ["patient"]}, {"response": "200"},],
+        "delete_api_v1_patient_patient_id": [{"parameters": ["patient"]}, {"response": "200"},],
+        "get_api_v1_patient_patient_id": [{"parameters": ["patient"]}, {"response": "200"},],
+        "get_api_v1_patient_patient_id_sensitive": [{"parameters": ["patient"]}, {"response": "200"},],
+        "get_api_v1_user_user_id": [{"parameters": ["user"]}, {"response": "200"}, ],
+        "get_api_v1_hospital_hospital_id": [{"parameters": ["hospital"]}, {"response": "200"}, ],
+
+    },
+]
+
+
