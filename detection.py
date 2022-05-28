0540e9b4dc410da702b1d00d2c2944841c6bc0c7
Lola Barberan Baeta <lolabarberan1@gmail.com>
2022-05-29 00:05:27 +0200

    finished

--- detection.py
+++ detection.py
@@ -1,6 +1,26 @@
 import json
+import webbrowser
 
 from example import *
+
+f = open('website.html','w')
+
+mensaje = """<html>
+<head><link rel="stylesheet" type="text/css" href="estilo.css"></head>
+<body><table class="detection" id="detection">
+  <tr>
+    <th>USER</th>
+    <th>FUNCTION</th>
+    <th>PARAMETERS</th>
+    <th>EXPECTED CODE</th>
+    <th>RESULT CODE </th>
+    <th>CHECK IF BODY CONTAINS</th>
+    <th>DOES IT CONTAIN IT?</th>
+    <th>RESULT</th>
+  </tr> """
+
+
+
 
 with open('data.json') as json_file:
     data = json.load(json_file)
@@ -15,6 +35,86 @@ for u in users_detection:
                         if par == info['function']:
                             if info["parameters"] == b["parameters"]:
                                 if info["code"] == b["response"]:
-                                    print("Entering with " + u + ": the function " + info["function"] + " with parameters " + str(info["parameters"]) + " has the correct code: " + b["response"] + ".")
-                                else: print("Entering with " + u + ": the function " + info["function"] + " with parameters " + str(info["parameters"]) + " has the code " + info["code"] + " and it had to be a " + b["response"] + ".")
+                                    if "message" in b and "body" in info:
+                                        if b["message"] in info["body"]:
+                                            mensaje = mensaje + """
+                                            <tr>
+                                                <td>""" + u + """</td>
+                                                <td>""" + info["function"] + """</td>
+                                                <td>""" + str(info["parameters"]) + """</td>
+                                                <td>""" + b["response"] + """</td>
+                                                <td>""" + info["code"] + """</td>
+                                                <td>""" + b["message"] + """</td>
+                                                <td>True</td>
+                                                <td id="green"></td>
+                                            </tr>"""
+                                        else:
+                                            mensaje = mensaje + """
+                                            <tr>
+                                                <td>""" + u + """</td>
+                                                <td>""" + info["function"] + """</td>
+                                                <td>""" + str(info["parameters"]) + """</td>
+                                                <td>""" + b["response"] + """</td>
+                                                <td>""" + info["code"] + """</td>
+                                                <td>""" + b["message"] + """</td>
+                                                <td>False</td>
+                                                <td id="red"></td>
+                                            </tr>"""
+                                    else:
+                                        mensaje = mensaje + """
+                                        <tr>
+                                            <td>""" + u + """</td>
+                                            <td>""" + info["function"] + """</td>
+                                            <td>""" + str(info["parameters"]) + """</td>
+                                            <td>""" + b["response"] + """</td>
+                                            <td>""" + info["code"] + """</td>
+                                            <td>---</td>
+                                            <td>---</td>
+                                            <td id="green"></td>
+                                        </tr>"""
+                                else:
+                                    if "message" in b and "body" in info:
+                                        if b["message"] in info["body"]:
+                                            mensaje = mensaje + """
+                                            <tr>
+                                                <td>""" + u + """</td>
+                                                <td>""" + info["function"] + """</td>
+                                                <td>""" + str(info["parameters"]) + """</td>
+                                                <td>""" + b["response"] + """</td>
+                                                <td>""" + info["code"] + """</td>
+                                                <td>""" + b["message"] + """</td>
+                                                <td>True</td>
+                                                <td id="red"></td>
+                                            </tr>"""
+                                        else:
+                                            mensaje = mensaje + """
+                                            <tr>
+                                                <td>""" + u + """</td>
+                                                <td>""" + info["function"] + """</td>
+                                                <td>""" + str(info["parameters"]) + """</td>
+                                                <td>""" + b["response"] + """</td>
+                                                <td>""" + info["code"] + """</td>
+                                                <td>""" + b["message"] + """</td>
+                                                <td>False</td>
+                                                <td id="red"></td>
+                                            </tr>"""
+                                    else:
+                                        mensaje = mensaje + """
+                                        <tr>
+                                            <td>""" + u + """</td>
+                                            <td>""" + info["function"] + """</td>
+                                            <td>""" + str(info["parameters"]) + """</td>
+                                            <td>""" + b["response"] + """</td>
+                                            <td>""" + info["code"] + """</td>
+                                            <td>---</td>
+                                            <td>---</td>
+                                            <td id="red"></td>
+                                        </tr>"""
                                 break
+
+mensaje = mensaje + """ </table> </body>
+</html> """
+f.write(mensaje)
+f.close()
+
+webbrowser.open_new_tab('website.html')
