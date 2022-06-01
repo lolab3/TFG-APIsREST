e75c8e978fb7fe18b97a2b4e174cef82a37d262c
Lola Barberan Baeta <lolabarberan1@gmail.com>
2022-06-01 11:28:18 +0200

    finished

--- detection.py
+++ detection.py
@@ -1,42 +1,68 @@
 import json
 import webbrowser
 
-from example import *
+#TO CALL TEXT FROM example.py
+from detectionData import *
 
-f = open('website.html','w')
+def detection(outputWebsite):
+    f = open(outputWebsite,'w')
 
-mensaje = """<html>
-<head><link rel="stylesheet" type="text/css" href="estilo.css"></head>
-<body><table class="detection" id="detection">
-  <tr>
-    <th>USER</th>
-    <th>FUNCTION</th>
-    <th>PARAMETERS</th>
-    <th>EXPECTED CODE</th>
-    <th>RESULT CODE </th>
-    <th>CHECK IF BODY CONTAINS</th>
-    <th>DOES THE BODY CONTAIN IT?</th>
-    <th>RESULT</th>
-  </tr> """
+    mensaje = """<html>
+    <head><link rel="stylesheet" type="text/css" href="estilo.css"></head>
+    <body><table class="detection" id="detection">
+      <tr>
+        <th>USER</th>
+        <th>FUNCTION</th>
+        <th>PARAMETERS</th>
+        <th>EXPECTED CODE</th>
+        <th>RESULT CODE </th>
+        <th>CHECK IF BODY CONTAINS</th>
+        <th>DOES THE BODY CONTAIN IT?</th>
+        <th>RESULT</th>
+      </tr> """
 
 
 
 
-with open('data.json') as json_file:
-    data = json.load(json_file)
+    with open('data.json') as json_file:
+        data = json.load(json_file)
 
-for u in users_detection:
-    x = globals()[u]
-    for info in data:
-        if info["username"] == u:
-            for inside in x:
-                for par,c in inside.items():
-                    for b in c:
-                        if par == info['function']:
-                            if info["parameters"] == b["parameters"]:
-                                if info["code"] == b["response"]:
-                                    if "message" in b and "body" in info:
-                                        if b["message"] in info["body"]:
+    for u in users_detection:
+        x = globals()[u]
+        for info in data:
+            if info["username"] == u:
+                for inside in x:
+                    for par,c in inside.items():
+                        for b in c:
+                            if par == info['function']:
+                                if info["parameters"] == b["parameters"]:
+                                    if info["code"] == b["response"]:
+                                        if "message" in b and "body" in info:
+                                            if b["message"] in info["body"]:
+                                                mensaje = mensaje + """
+                                                <tr>
+                                                    <td>""" + u + """</td>
+                                                    <td>""" + info["function"] + """</td>
+                                                    <td>""" + str(info["parameters"]) + """</td>
+                                                    <td>""" + b["response"] + """</td>
+                                                    <td>""" + info["code"] + """</td>
+                                                    <td>""" + b["message"] + """</td>
+                                                    <td>True</td>
+                                                    <td id="green">GOOD</td>
+                                                </tr>"""
+                                            else:
+                                                mensaje = mensaje + """
+                                                <tr>
+                                                    <td>""" + u + """</td>
+                                                    <td>""" + info["function"] + """</td>
+                                                    <td>""" + str(info["parameters"]) + """</td>
+                                                    <td>""" + b["response"] + """</td>
+                                                    <td>""" + info["code"] + """</td>
+                                                    <td>""" + b["message"] + """</td>
+                                                    <td>False</td>
+                                                    <td id="red">BAD</td>
+                                                </tr>"""
+                                        else:
                                             mensaje = mensaje + """
                                             <tr>
                                                 <td>""" + u + """</td>
@@ -44,10 +70,36 @@ for u in users_detection:
                                                 <td>""" + str(info["parameters"]) + """</td>
                                                 <td>""" + b["response"] + """</td>
                                                 <td>""" + info["code"] + """</td>
-                                                <td>""" + b["message"] + """</td>
-                                                <td>True</td>
+                                                <td>---</td>
+                                                <td>---</td>
                                                 <td id="green">GOOD</td>
                                             </tr>"""
+                                    else:
+                                        if "message" in b and "body" in info:
+                                            if b["message"] in info["body"]:
+                                                mensaje = mensaje + """
+                                                <tr>
+                                                    <td>""" + u + """</td>
+                                                    <td>""" + info["function"] + """</td>
+                                                    <td>""" + str(info["parameters"]) + """</td>
+                                                    <td>""" + b["response"] + """</td>
+                                                    <td>""" + info["code"] + """</td>
+                                                    <td>""" + b["message"] + """</td>
+                                                    <td>True</td>
+                                                    <td id="red">BAD</td>
+                                                </tr>"""
+                                            else:
+                                                mensaje = mensaje + """
+                                                <tr>
+                                                    <td>""" + u + """</td>
+                                                    <td>""" + info["function"] + """</td>
+                                                    <td>""" + str(info["parameters"]) + """</td>
+                                                    <td>""" + b["response"] + """</td>
+                                                    <td>""" + info["code"] + """</td>
+                                                    <td>""" + b["message"] + """</td>
+                                                    <td>False</td>
+                                                    <td id="red">BAD</td>
+                                                </tr>"""
                                         else:
                                             mensaje = mensaje + """
                                             <tr>
@@ -56,65 +108,17 @@ for u in users_detection:
                                                 <td>""" + str(info["parameters"]) + """</td>
                                                 <td>""" + b["response"] + """</td>
                                                 <td>""" + info["code"] + """</td>
-                                                <td>""" + b["message"] + """</td>
-                                                <td>False</td>
+                                                <td>---</td>
+                                                <td>---</td>
                                                 <td id="red">BAD</td>
                                             </tr>"""
-                                    else:
-                                        mensaje = mensaje + """
-                                        <tr>
-                                            <td>""" + u + """</td>
-                                            <td>""" + info["function"] + """</td>
-                                            <td>""" + str(info["parameters"]) + """</td>
-                                            <td>""" + b["response"] + """</td>
-                                            <td>""" + info["code"] + """</td>
-                                            <td>---</td>
-                                            <td>---</td>
-                                            <td id="green">GOOD</td>
-                                        </tr>"""
-                                else:
-                                    if "message" in b and "body" in info:
-                                        if b["message"] in info["body"]:
-                                            mensaje = mensaje + """
-                                            <tr>
-                                                <td>""" + u + """</td>
-                                                <td>""" + info["function"] + """</td>
-                                                <td>""" + str(info["parameters"]) + """</td>
-                                                <td>""" + b["response"] + """</td>
-                                                <td>""" + info["code"] + """</td>
-                                                <td>""" + b["message"] + """</td>
-                                                <td>True</td>
-                                                <td id="red">BAD</td>
-                                            </tr>"""
-                                        else:
-                                            mensaje = mensaje + """
-                                            <tr>
-                                                <td>""" + u + """</td>
-                                                <td>""" + info["function"] + """</td>
-                                                <td>""" + str(info["parameters"]) + """</td>
-                                                <td>""" + b["response"] + """</td>
-                                                <td>""" + info["code"] + """</td>
-                                                <td>""" + b["message"] + """</td>
-                                                <td>False</td>
-                                                <td id="red">BAD</td>
-                                            </tr>"""
-                                    else:
-                                        mensaje = mensaje + """
-                                        <tr>
-                                            <td>""" + u + """</td>
-                                            <td>""" + info["function"] + """</td>
-                                            <td>""" + str(info["parameters"]) + """</td>
-                                            <td>""" + b["response"] + """</td>
-                                            <td>""" + info["code"] + """</td>
-                                            <td>---</td>
-                                            <td>---</td>
-                                            <td id="red">BAD</td>
-                                        </tr>"""
-                                break
+                                    break
 
-mensaje = mensaje + """ </table> </body>
-</html> """
-f.write(mensaje)
-f.close()
+    mensaje = mensaje + """ </table> </body>
+    </html> """
+    f.write(mensaje)
+    f.close()
 
-webbrowser.open_new_tab('website.html')
+    webbrowser.open_new_tab(outputWebsite)
+
+detection('website.html')
--- scanAPI.py
+++ scanAPI.py
@@ -1,11 +1,12 @@
 import inspect
 import re
 import json
-#import OpenApi as OpenApi
-
-from ApiClientMethods import *
-from sampleData import *
 from itertools import combinations
+from ApiClientMethods import *
+
+#TO CALL TEXT FROM sampleData.py
+from scanApiData import *
+
 
 Functions = []
 allFunctions = []
@@ -16,121 +17,124 @@ allUsers = []
 data = {"functions": []}
 data_holder = data["functions"]
 
+def scanAPI(inputFile, outputFile):
 
-#Buscar totes les funcions al document apiClientMethods.py
-f = open ('apiClientMethods.py','r')
-message = f.read()
+    #Buscar totes les funcions al document apiClientMethods.py
+    f = open(inputFile, 'r')
+    message = f.read()
 
-allFunctions = re.findall("def .*", message)
-for i in allFunctions:
-    Functions.append(i)
+    allFunctions = re.findall("def .*", message)
+    for i in allFunctions:
+        Functions.append(i)
 
-words = [w.replace("def ", "") for w in Functions]
-for i in words:
-    for j in i:
-        boolean = j.find(str('('))
-        if boolean == 0:
-            listofDelete.append(i.index(j))
+    words = [w.replace("def ", "") for w in Functions]
+    for i in words:
+        for j in i:
+            boolean = j.find(str('('))
+            if boolean == 0:
+                listofDelete.append(i.index(j))
 
-boolean = False
-for i in words:
-    a = words.index(i)
-    for j in i:
-        if(boolean == False):
-            if i.index(j) == listofDelete[words.index(i)]:
-                x = listofDelete[words.index(i)]
-                boolean = True
-        if boolean:
-            i = i[:x] + i[x + 1:]
-
-    finalList.append(i)
     boolean = False
+    for i in words:
+        a = words.index(i)
+        for j in i:
+            if(boolean == False):
+                if i.index(j) == listofDelete[words.index(i)]:
+                    x = listofDelete[words.index(i)]
+                    boolean = True
+            if boolean:
+                i = i[:x] + i[x + 1:]
 
-#print(finalList)
+        finalList.append(i)
+        boolean = False
 
-#identificar funció Login
-indexofLogin = -1
+    #print(finalList)
 
-#intentar trobar la funció de login
-for i, parameter in enumerate(finalList):
-    if 'login' in parameter or 'Login' in parameter or 'LOGIN' in parameter:
-        indexofLogin = i
+    #identificar funció Login
+    indexofLogin = -1
 
-#si no, demanar identificar-la
-if indexofLogin == -1:
-    print("Quina funció és la de login?")
+    #intentar trobar la funció de login
     for i, parameter in enumerate(finalList):
-        print((i+1), ". ", parameter)
-    i = int(input()) - 1
+        if 'login' in parameter or 'Login' in parameter or 'LOGIN' in parameter:
+            indexofLogin = i
 
-funcioLogin = finalList[indexofLogin]
-funcioLogin = globals()[funcioLogin]
+    #si no, demanar identificar-la
+    if indexofLogin == -1:
+        print("Quina funció és la de login?")
+        for i, parameter in enumerate(finalList):
+            print((i+1), ". ", parameter)
+        i = int(input()) - 1
+
+    funcioLogin = finalList[indexofLogin]
+    funcioLogin = globals()[funcioLogin]
 
 
-#GetAllUsers
-for user in users:
-    u = {'username': user['username'], 'password': user['password']}
-    allUsers.append(u)
+    #GetAllUsers
+    for user in users:
+        u = {'username': user['username'], 'password': user['password']}
+        allUsers.append(u)
 
-#print(allUsers)
+    #print(allUsers)
 
 
-#Login with all Users
-for u in allUsers:
-    code, body = funcioLogin(user=u)
-    token = json.loads(body)
+    #Login with all Users
+    for u in allUsers:
+        code, body = funcioLogin(user=u)
+        token = json.loads(body)
 
-    for i, function in enumerate(finalList):
-        if i != indexofLogin:
-            params = []
-            diffpam2 = []
-            final = []
-            combor = []
-            funcio = globals()[function]
-            args = inspect.getfullargspec(funcio)
-            total_args_id = args[0].__len__() - 2
-            if "id" in function:
-                for i in args[0]:
-                    if "id" in i:
-                        p = i.replace("_id", "") + "s"
-                        params.append(p)
-                for i, x in enumerate(params):
-                    y = globals()[x]
-                    for h in y:
-                        diffpam2.append(h['id'])
+        for i, function in enumerate(finalList):
+            if i != indexofLogin:
+                params = []
+                diffpam2 = []
+                final = []
+                combor = []
+                funcio = globals()[function]
+                args = inspect.getfullargspec(funcio)
+                total_args_id = args[0].__len__() - 2
+                if "id" in function:
+                    for i in args[0]:
+                        if "id" in i:
+                            p = i.replace("_id", "") + "s"
+                            params.append(p)
+                    for i, x in enumerate(params):
+                        y = globals()[x]
+                        for h in y:
+                            diffpam2.append(h['id'])
 
-                for combo in combinations(diffpam2, total_args_id):
-                    combor = []
-                    for x in combo:
-                        combor.append(''.join([i for i in x if not i.isdigit()]))
-                    if len(combor) == len(set(combor)):
-                        code, body = funcio(token['token'], None, *combo)
-                        data_holder.append({'function': function,
-                                            'username': u['username'],
-                                            'parameters': combo,
-                                            'token': token['token'],
-                                            'code': str(code),
-                                            'body': body})
-            else:
-                if (args[0].__len__() == 2) and (args[0][1] == "body"):
-                    code, body = funcio(token['token'])
-                    data_holder.append({'function': function,
-                                        'username': u['username'],
-                                        'parameters': None,
-                                        'token': token['token'],
-                                        'code': str(code),
-                                        'body': body})
+                    for combo in combinations(diffpam2, total_args_id):
+                        combor = []
+                        for x in combo:
+                            combor.append(''.join([i for i in x if not i.isdigit()]))
+                        if len(combor) == len(set(combor)):
+                            code, body = funcio(token['token'], None, *combo)
+                            data_holder.append({'function': function,
+                                                'username': u['username'],
+                                                'parameters': combo,
+                                                'token': token['token'],
+                                                'code': str(code),
+                                                'body': body})
                 else:
-                    param = args[0][1] + "s"
-                    params = globals()[param]
-                    for p in params:
-                        code, body = funcio(token['token'], p)
+                    if (args[0].__len__() == 2) and (args[0][1] == "body"):
+                        code, body = funcio(token['token'])
                         data_holder.append({'function': function,
                                             'username': u['username'],
-                                            'parameters': p['id'],
+                                            'parameters': None,
                                             'token': token['token'],
                                             'code': str(code),
                                             'body': body})
+                    else:
+                        param = args[0][1] + "s"
+                        params = globals()[param]
+                        for p in params:
+                            code, body = funcio(token['token'], p)
+                            data_holder.append({'function': function,
+                                                'username': u['username'],
+                                                'parameters': p['id'],
+                                                'token': token['token'],
+                                                'code': str(code),
+                                                'body': body})
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data_holder, f, ensure_ascii=False, indent=4)
+    with open(outputFile, 'w', encoding='utf-8') as f:
+        json.dump(data_holder, f, ensure_ascii=False, indent=4)
+
+scanAPI('apiClientMethods.py', 'data.json')
